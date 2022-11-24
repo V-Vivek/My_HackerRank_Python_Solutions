bafae6fd8b4adaee59282682a77b75a2d4131413
Vivek Jadhav <117569148+V-Vivek@users.noreply.github.com>
2022-11-24 22:36:33 +0530

    Create leap_year.py

---
+++ leap_year.py
@@ -0,0 +1,14 @@
+# Question : https://www.hackerrank.com/challenges/write-a-function/problem?isFullScreen=true
+def is_leap(year):
+    leap = False
+    # To check century leap years
+    if (year % 400 == 0) and (year % 100 == 0):
+        leap = True
+    # To check non-century leap years
+    elif (year % 4 ==0) and (year % 100 != 0):
+        leap = True
+    
+    return leap
+
+year = int(input())
+print(is_leap(year))

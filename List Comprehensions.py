b47e2c221cf478788459421a8c98ec6527d4cb41
V-Vivek <jadhav.vivek.18.3@gmail.com>
2022-11-26 18:55:54 +0530

    Modified some errors

--- List Comprehensions.py
+++ List Comprehensions.py
@@ -1,12 +1,11 @@
 # Question : https://www.hackerrank.com/challenges/list-comprehensions/problem
-
-if __name__ == '__main__':
-	x = int(input())
-    y = int(input())
-    z = int(input())
-    n = int(input())
+#  
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
     
-    # Note that space sepearted multiple list comprehesions are used to get the coordinates
-    result = [[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i + j + k != n]
+# Note that space sepearted multiple list comprehesions are used to get the coordinates
+result = [[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i + j + k != n]
 	
 print(result)

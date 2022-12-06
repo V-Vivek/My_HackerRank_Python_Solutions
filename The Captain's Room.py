b08c0add6fb15e336abed459314b6b73d8d389ab
V-Vivek <jadhav.vivek.18.3@gmail.com>
2022-12-06 20:30:26 +0530

    Solution

--- The Captain's Room.py
+++ The Captain's Room.py
@@ -1,5 +1,6 @@
 # Question: https://www.hackerrank.com/challenges/py-the-captains-room/problem?isFullScreen=true
 
+# Solution #1: Takes more time to execute
 k = int(input())
 room_number_list = list(map(int, input().split()))
 unique_rooms = list(set(room_number_list))
@@ -7,3 +8,14 @@ unique_rooms = list(set(room_number_list))
 for _ in unique_rooms:
     if room_number_list.count(_) != k:
         print(_)
+
+# Optimized solution:
+
+from collections import Counter
+
+k = int(input())
+room_count = dict(Counter(map(int, input().split())))
+
+for k,v in room_count.items():
+    if v == 1:
+        print(k)
--- test.py
+++ test.py
@@ -1,7 +1,13 @@
-k = 5
-room_number_list = [1, 2, 3, 6, 5, 4, 4, 2, 5, 3, 6, 1, 6, 5, 3, 2, 4, 1, 2, 5, 1, 4, 3, 6, 8, 4, 3, 1, 5, 6, 2]
-unique_rooms = list(set(room_number_list))
+# k = 5
+# room_number_list = [1, 2, 3, 6, 5, 4, 4, 2, 5, 3, 6, 1, 6, 5, 3, 2, 4, 1, 2, 5, 1, 4, 3, 6, 8, 4, 3, 1, 5, 6, 2]
 
-for _ in unique_rooms:
-    if room_number_list.count(_) != k:
-        print(_)
+
+from collections import Counter
+
+k = int(input())
+room_count = dict(Counter(map(int, input().split())))
+print(room_count)
+
+for k,v in room_count.items():
+    if v == 1:
+        print(k)

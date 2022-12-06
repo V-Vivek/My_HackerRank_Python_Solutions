5679089ef1db8cdeb65469be691efbbc734f969c
V-Vivek <jadhav.vivek.18.3@gmail.com>
2022-12-06 08:43:29 +0530

    Solution

---
+++ Set .symmetric_difference() Operation.py
@@ -0,0 +1,16 @@
+# Question: https://www.hackerrank.com/challenges/py-set-symmetric-difference-operation/problem?isFullScreen=true
+
+n = int(input())
+english = set(map(int, input().split()))
+b = int(input())
+french = set(map(int, input().split()))
+
+# Solution #1
+
+english = english.symmetric_difference(french)
+print(len(english))
+
+# Solution #2
+
+# english = (english - french).union(french - english)
+# print(len(english))
--- test.py
+++ test.py
@@ -3,10 +3,12 @@ english = set(map(int, input().split()))
 b = int(input())
 french = set(map(int, input().split()))
 
-# english = english.difference(french)
+# Solution #1
+
+# english = english.symmetric_difference(french)
 # print(len(english))
 
 # Solution #2
 
-english = english - french
+english = (english - french).union(french - english)
 print(len(english))

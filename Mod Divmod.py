ac051547949f7400bc435beb8c8eba2d162726ab
V-Vivek <jadhav.vivek.18.3@gmail.com>
2022-12-07 08:04:39 +0530

    Solution

---
+++ Mod Divmod.py
@@ -0,0 +1,8 @@
+# Question: https://www.hackerrank.com/challenges/python-mod-divmod/problem
+
+a = int(input())
+b = int(input())
+
+print(divmod(a, b)[0])
+print(divmod(a, b)[1])
+print(divmod(a, b))

d0fe286d350d98ef2d33c78957ad309b836d8173
V-Vivek <jadhav.vivek.18.3@gmail.com>
2022-12-08 19:59:37 +0530

    Solution

---
+++ The Minion Game.py
@@ -0,0 +1,23 @@
+# Question: https://www.hackerrank.com/challenges/the-minion-game/problem?isFullScreen=true
+
+def minion_game(string):
+    s=len(string)
+    vowel = 0
+    consonant = 0
+     
+    for i in range(s):
+        if string[i] in 'AEIOU':
+           vowel += (s-i)
+        else:
+           consonant += (s-i)
+                
+    if vowel < consonant:
+        print('Stuart ' + str(consonant))
+    elif vowel > consonant:
+        print('Kevin ' + str(vowel))
+    else:
+        print('Draw')
+
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
--- test.py
+++ test.py
@@ -1,6 +1,27 @@
-a = int(input())
-b = int(input())
-c = int(input())
-d = int(input())
+def minion_game(string):
 
-print(a**b + c**d)
+    vowels = ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u']
+    vowels_score = 0
+    consonants_score = 0
+    l = len(string)
+
+    for i in range(l):
+        if string[i] in vowels:
+            vowels_score += len(string[i:l])
+        else:
+            consonants_score += len(string[i:l])
+
+
+    if consonants_score > vowels_score:
+        print('Stuart', consonants_score)
+    elif consonants_score < vowels_score:
+        print('Kevin', vowels_score)
+    else:
+        print('Draw')
+    print(vowels_score)
+    print(consonants_score)
+
+
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)

615961ed19afbe2e209319c2403144b11d655282
V-Vivek <jadhav.vivek.18.3@gmail.com>
2022-12-12 22:32:19 +0530

    Optimized

--- Collections.namedtuple().py
+++ Collections.namedtuple().py
@@ -5,20 +5,10 @@ from collections import namedtuple
 total = 0
 N = int(input()) 
 columns = input()
-for i, column_name in enumerate(columns.split()):
-    if column_name == 'ID':
-        id = i
-    elif column_name == 'MARKS':
-        marks = i
-    elif column_name == 'CLASS':
-        cls = i
-    else:
-        name = i
-
 student = namedtuple('student', columns)
 for _ in range(N):
     a, b, c, d = input().split()
     data = student(a, b, c, d)
-    total += int(data[marks])
+    total += int(data.MARKS)
 
 print(round(total/N, 2))

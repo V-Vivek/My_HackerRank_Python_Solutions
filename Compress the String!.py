96d947469fadd44c153ce1a8050f1fd0c03c0899
V-Vivek <jadhav.vivek.18.3@gmail.com>
2022-12-15 07:29:52 +0530

    Optimized

--- Compress the String!.py
+++ Compress the String!.py
@@ -3,18 +3,14 @@
 s = input()
 
 i = 0
-while i<len(s):
+while i < len(s):
     j = i + 1
     count = 1
-    if j < len(s):
-        while s[j] == s[i]:
+    while j < len(s):
+        if s[j] == s[i]:
             count += 1
-            if j+1 < len(s):
-                j += 1
-            else:
-                break
-    if s[i-1] == s[i] and i != 0:
-        pass
-    else:
-        print(tuple([count, int(s[i])]), end = ' ')
+            j += 1
+        else:
+            break
+    print(tuple([count, int(s[i])]), end = ' ')
     i = j
--- test.py
+++ test.py
@@ -1,19 +1,14 @@
-s = '123456789'
+s = '122344556'
 
 i = 0
-while i<len(s):
+while i < len(s):
     j = i + 1
     count = 1
-    if j < len(s):
-        while s[j] == s[i]:
+    while j < len(s):
+        if s[j] == s[i]:
             count += 1
-            if j+1 < len(s):
-                j += 1
-            else:
-                break
-    if s[i-1] == s[i]:
-        pass
-    else:
-        print(tuple([count, int(s[i])]), end = ' ')
-    # print(i)
+            j += 1
+        else:
+            break
+    print(tuple([count, int(s[i])]), end = ' ')
     i = j
